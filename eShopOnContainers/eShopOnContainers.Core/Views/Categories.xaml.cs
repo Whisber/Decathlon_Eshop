cadc0606e6e5f3aa4c1c20d30c65ea704a150251
“Oguzhanozturkkk” <oguzhanozturk11@gmail.com>
2022-06-13 09:25:31 +0300

    kategoriler eklendi

---
+++ eShopOnContainers/eShopOnContainers.Core/Views/Categories.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace eShopOnContainers.Core.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Categories : ContentPage
+    {
+        public Categories()
+        {
+            InitializeComponent();
+        }
+        private async void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ImageButton_Clicked_1(object sender, EventArgs e)
+        {
+
+           
+
+        }
+
+        private void ImageButton_Clicked_2(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
