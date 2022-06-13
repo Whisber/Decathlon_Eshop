f05cff98a4e947832fc64f99d0c527e427b8a88e
“Oguzhanozturkkk” <oguzhanozturk11@gmail.com>
2022-06-13 09:06:11 +0300

    account wiew eklendi

---
+++ eShopOnContainers/eShopOnContainers.Core/Views/Account.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace eShopOnContainers.Core.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Account : ContentPage
+    {
+        public Account()
+        {
+            InitializeComponent();
+        }
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HomePage());
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
