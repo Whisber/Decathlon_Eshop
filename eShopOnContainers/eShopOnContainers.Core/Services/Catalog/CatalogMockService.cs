42b288f26b71e9aef726ebed73808fa322f6cc6b
Whisber <Whisber1@gmail.com>
2022-06-21 22:09:12 +0300

    Servisler ile ürünler eklendi filtreleme yapıldı.

--- eShopOnContainers/eShopOnContainers.Core/Services/Catalog/CatalogMockService.cs
+++ eShopOnContainers/eShopOnContainers.Core/Services/Catalog/CatalogMockService.cs
@@ -12,23 +12,24 @@ namespace eShopOnContainers.Core.Services.Catalog
     {
         private ObservableCollection<CatalogBrand> MockCatalogBrand = new ObservableCollection<CatalogBrand>
         {
-            new CatalogBrand { Id = 1, Brand = "Azure" },
-            new CatalogBrand { Id = 2, Brand = "Visual Studio" }
+            new CatalogBrand { Id = 1, Brand = "Salcano" },
+            new CatalogBrand { Id = 2, Brand = "Quecho" },
+            new CatalogBrand { Id = 3, Brand = "Umit bisiklet" }
         };
 
         private ObservableCollection<CatalogType> MockCatalogType = new ObservableCollection<CatalogType>
         {
-            new CatalogType { Id = 1, Type = "Mug" },
-            new CatalogType { Id = 2, Type = "T-Shirt" }
+            new CatalogType { Id = 1, Type = "Bisiklet" },
+            new CatalogType { Id = 2, Type = "Cadir" }
         };
 
         private ObservableCollection<CatalogItem> MockCatalog = new ObservableCollection<CatalogItem>
         {
-            new CatalogItem { Id = Common.Common.MockCatalogItemId01, PictureUri = "fake_product_01.png", Name = ".NET Bot Blue Sweatshirt (M)", Price = 19.50M, CatalogBrandId = 2, CatalogBrand = "Visual Studio", CatalogTypeId = 2, CatalogType = "T-Shirt" },
-            new CatalogItem { Id = Common.Common.MockCatalogItemId02, PictureUri = "fake_product_02.png", Name = ".NET Bot Purple Sweatshirt (M)", Price = 19.50M, CatalogBrandId = 2, CatalogBrand = "Visual Studio", CatalogTypeId = 2, CatalogType = "T-Shirt" },
-            new CatalogItem { Id = Common.Common.MockCatalogItemId03, PictureUri = "fake_product_03.png", Name = ".NET Bot Black Sweatshirt (M)", Price = 19.95M, CatalogBrandId = 2, CatalogBrand = "Visual Studio", CatalogTypeId = 2, CatalogType = "T-Shirt" },
-            new CatalogItem { Id = Common.Common.MockCatalogItemId04, PictureUri = "fake_product_04.png", Name = ".NET Black Cupt", Price = 17.00M, CatalogBrandId = 2, CatalogBrand = "Visual Studio", CatalogTypeId = 1, CatalogType = "Mug" },
-            new CatalogItem { Id = Common.Common.MockCatalogItemId05, PictureUri = "fake_product_05.png", Name = "Azure Black Sweatshirt (M)", Price = 19.50M, CatalogBrandId = 1, CatalogBrand = "Azure", CatalogTypeId = 2, CatalogType = "T-Shirt" }
+            new CatalogItem { Id = Common.Common.MockCatalogItemId01, PictureUri = "KampCadir1.png", Name = "Cadir1(S)", Price = 30.00M, CatalogBrandId = 2, CatalogBrand = "Decathlon", CatalogTypeId = 2, CatalogType = "Cadir" },
+            new CatalogItem { Id = Common.Common.MockCatalogItemId02, PictureUri = "KampCadir2.png", Name = "Cadir2(M)", Price = 45.00M, CatalogBrandId = 2, CatalogBrand = "Decathlon", CatalogTypeId = 2, CatalogType = "Cadir" },
+            new CatalogItem { Id = Common.Common.MockCatalogItemId03, PictureUri = "KampCadir3.png", Name = "Cadir3(L)", Price = 70.00M, CatalogBrandId = 2, CatalogBrand = "Decathlon", CatalogTypeId = 2, CatalogType = "Cadir" },
+            new CatalogItem { Id = Common.Common.MockCatalogItemId04, PictureUri = "BisikletEkle1.jpg", Name = "21 Vites bisiklet", Price = 180.00M, CatalogBrandId = 1, CatalogBrand = "Salcano", CatalogTypeId = 1, CatalogType = "Bisiklet" },
+            new CatalogItem { Id = Common.Common.MockCatalogItemId05, PictureUri = "BisikletEkle2.jpg", Name = "18 Vites bisiklet", Price = 160.50M, CatalogBrandId = 3, CatalogBrand = "Umit bisiklet", CatalogTypeId = 1, CatalogType = "Bisiklet" }
         };
 
         public async Task<ObservableCollection<CatalogItem>> GetCatalogAsync()
