0cf99ed7833fbd66488c12056de5daf146d313df
Whisber <Whisber1@gmail.com>
2022-06-21 20:38:18 +0300

    Cadir services eklendi

---
+++ eShopOnContainers/eShopOnContainers.Core/Services/KampCadir/ICadirService.cs
@@ -0,0 +1,17 @@
+﻿using eShopOnContainers.Core.Models.Catalog;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopOnContainers.Core.Services.KampCadir
+{
+    public interface ICadirService
+    {
+        Task<ObservableCollection<CatalogBrand>> GetCatalogBrandAsync();
+        Task<ObservableCollection<CatalogItem>> FilterAsync(int catalogBrandId, int catalogTypeId);
+        Task<ObservableCollection<CatalogType>> GetCatalogTypeAsync();
+        Task<ObservableCollection<CatalogItem>> GetCatalogAsync();
+    }
+}
