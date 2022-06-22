010747ecc35f7ccbf7e350731c8aed11f1614f02
Whisber <Whisber1@gmail.com>
2022-06-23 01:29:06 +0300

    Arama için FiltersViewModel eklendi.

---
+++ eShopOnContainers/eShopOnContainers.Core/ViewModels/FavoritiesViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using eShopOnContainers.Core.ViewModels;
+using eShopOnContainers.Core.Views;
+using eShopOnContainers.Core.ViewModels.Base;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace eShopOnContainers.Core.ViewModels
+{
+    public class FavoritiesViewModel
+    {
+       
+    }
+}
---
+++ eShopOnContainers/eShopOnContainers.Core/ViewModels/FiltersViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using eShopOnContainers.Core.ViewModels;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.UI.Views;
+
+namespace eShopOnContainers.Core.ViewModels
+{
+    public class FiltersViewModel : Popup
+    {
+        public FiltersViewModel()
+        {
+            
+        }
+        protected override object GetLightDismissResult()
+        {
+            return GetLightDismissResult();
+        }
+        public void OnFilterClicked(System.Object sender, System.EventArgs e)
+        {
+            if (BindingContext is CadirViewModel viewModel)
+            {
+                viewModel.FilterCommand.Execute(null);
+                this.Dismiss(true);
+            }
+        }
+    }
+}
